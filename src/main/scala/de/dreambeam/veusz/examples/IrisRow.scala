ed7ddfce047a504298e948ef09baac2ee432def4
Julian Eger-Benninger <julian.eger-benninger@it-designers.de>
2019-02-22 12:20:00 +0100

    Removed examples

--- src/main/scala/de/dreambeam/veusz/examples/IrisRow.scala
+++
@@ -1,7 +0,0 @@
-package de.dreambeam.veusz.examples
-
-case class IrisRow(sepalLength: Double,
-                   sepalWidth: Double,
-                   petalLength: Double,
-                   petalWidth: Double,
-                   name: String)
